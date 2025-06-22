3646516c0293aeedf6f21062a0cacb0a172ef7d0
Ignazio-Emanuele-Picciche <ignazioemanuele.picciche@gmail.com>
2025-06-22 21:57:05 +0200

    prepering dataset and videos

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,7 +1,22 @@
 from datasets import load_dataset
+import logging
+import os
+
+# Configura il logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+# Percorso per salvare il dataset
+output_path = "./data/raw/emosign_dataset"
 
 # Carica il dataset EmoSign
-dataset = load_dataset("catfang/emosign")
+logging.info("Caricamento del dataset 'catfang/emosign' da Hugging Face...")
+dataset = load_dataset("catfang/emosign", trust_remote_code=True)
+logging.info("Dataset caricato con successo.")
 
-# Salva il dataset
-dataset.save_to_disk("./data/raw/emosign_dataset")
+# Salva il dataset su disco
+logging.info(f"Salvataggio del dataset in '{output_path}'...")
+os.makedirs(os.path.dirname(output_path), exist_ok=True)
+dataset.save_to_disk(output_path)
+logging.info("Salvataggio del dataset completato.")
